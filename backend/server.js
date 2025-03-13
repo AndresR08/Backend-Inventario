4423d948502770207b342c9cf8f6e805bfab1841
AndresR08 <andresr22ro@gmail.com>
2025-03-13 10:44:02 +0000

    Agregamo 0.0.0.0 para aceptar conexiones externas

--- backend/server.js
+++ backend/server.js
@@ -79,6 +79,6 @@ app.use("/api/auth", authRoutes);
 
 // Configurar el puerto de la aplicación
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });

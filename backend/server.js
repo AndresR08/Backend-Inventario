9bc5c7f0362662ecc198b2f820aba56eca8cf60e
AndresR08 <andresr22ro@gmail.com>
2025-03-13 10:47:12 +0000

    Cambio de puerto a 5000

--- backend/server.js
+++ backend/server.js
@@ -78,7 +78,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
 // Configurar el puerto de la aplicación
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });

626a2ae8ef80907da345c4502594837936830e36
AndresR08 <andresr22ro@gmail.com>
2025-03-14 12:18:39 +0000

    Quitando temporalmente el authMiddleware de la ruta

--- backend/src/routes/saleRoutes.js
+++ backend/src/routes/saleRoutes.js
@@ -137,7 +137,7 @@ router.delete("/:id", authMiddleware, adminMiddleware, async (req, res) => {
 });
 
 // Ruta para obtener estadísticas de ventas
-router.get("/stats", authMiddleware, async (req, res) => {
+router.get("/stats", async (req, res) => {
   console.log("📊 Ruta /api/sales/stats fue llamada");
   try {
     // Contar el total de ventas

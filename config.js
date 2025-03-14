37d3a898da45ef6c61d49c080751393020f877f8
AndresR08 <andresr22ro@gmail.com>
2025-03-14 13:11:49 +0000

    Logs

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
-
+console.log("MONGO_URI =", process.env.MONGO_URI);
 const config = {
   mongoURI: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
   port: process.env.PORT || 5000,
   allowedOrigins: [
-    "http://localhost:5173",  // Permitir pruebas en local
-    "https://mellifluous-begonia-f48751.netlify.app", // Permitir Netlify
+    "http://localhost:5173",
+    "https://mellifluous-begonia-f48751.netlify.app",
   ],
 };
-
 module.exports = config;
